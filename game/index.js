32e8ecfc8e14ef81881e29d3df6770b089c2bc1f
Prince <prince@Princes-MacBook-Pro.local>
2018-07-05 16:59:30 -0400

    finished

--- game/index.js
+++ game/index.js
@@ -1,31 +1,44 @@
 import {Map} from 'immutable'
 
 import {createStore} from 'redux'
+import { stat } from 'fs';
 
 const gameBoard = Map()
 const MOVE = 'MOVE'
 
 export default function reducer(state={board : gameBoard, turn : 'O'}, action) {
-  console.log(action)
   if (action.type === MOVE) {
-  	console.log ('it works')
-  	state.board[row][column] = action.player
+		state.board = state.board.setIn(action.position,state.turn)
+		if(action.player === 'O'){
+		  state.turn = 'X'
+		}else {
+			state.turn = 'O'
+		}
   }
+	// console.log(state)
   return (state)
-
 }
-	
 //const store = createStore(reducer)
 
-function move (turn, coordinate) {
-	/*reducer.state*/console.log ('doot doot mr skeltal')
+export const move = function  (player, arr) {
+	/*reducer.state*/console.log (arr)
 	return {
-   type: MOVE,
-   position: coordinate,
-   player: turn
- }
+	type: MOVE,
+	position: arr,
+  player: player
 }
-  
+}
+	
+const convertCord = function(arr){
+	let output = 0;
+	if (arr[1] === 1){
+		output += 3;
+	} else if (arr[1] === 2){
+		output += 6
+	}
+	output += arr[0]
+	return output
+} 
   /*
 	const reducer (state) {
 		state = {
