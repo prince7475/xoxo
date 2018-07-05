18bc09c2a4f52eeaa3aa209c25cd90a58f693d8c
loren <lorenceye@gmail.com>
2018-07-05 15:42:35 -0400

    Switching

--- game/index.js
+++ game/index.js
@@ -1,6 +1,40 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+import {createStore} from 'redux'
+
+const gameBoard = Map()
+const MOVE = 'MOVE'
+
+export default function reducer(state={board : gameBoard, turn : 'O'}, action) {
+  console.log(action)
+  if (action.type === MOVE) {
+  	console.log ('it works')
+  	state.board[row][column] = action.player
+  }
+  return (state)
+
 }
+	
+//const store = createStore(reducer)
+
+function move (turn, coordinate) {
+	/*reducer.state*/console.log ('doot doot mr skeltal')
+	return {
+   type: MOVE,
+   position: coordinate,
+   player: turn
+ }
+}
+  
+  /*
+	const reducer (state) {
+		state = {
+  		currentPlayerTurn :  true
+  		gameBoard : [[null, null, null], [null, null, null], [null, null, null]]
+		}
+	}
+	
+  Move (playerBool, arr) {
+  	gameBoard[arr[1], arr[2]] = playerBool
+  }
+	*/
